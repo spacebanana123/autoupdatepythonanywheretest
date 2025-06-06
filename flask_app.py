ebb6473eb0628e95bbc92a49c0e741091ba1d93a
Matt Peng <mattyukepeng@gmail.com>
2025-06-05 22:59:16 -0500

    first commit

---
+++ flask_app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import git
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/update_server', methods=['POST'])
+def update_server_from_git():
+    if request.method != 'POST':
+        return 'OK'
+    else: 
+        event = request.headers.get('X-GitHub-Event')
+        if event == "ping":
+            return json.dumps({'msg': 'Hi!'})
+        if event != "push":
+            return json.dumps({'msg': "Wrong event type"})
+        repo = git.Repo('/var/www/sites/mysite')
+        origin = repo.remotes.origin
+
+        pull_info = origin.pull()
+
+        commit_hash = pull_info[0].commit.hexsha
+
+        build_commit = f'build_commit = "{commit_hash}"'
+        print(f'{build_commit}')
+        return 'Updated PythonAnywhere server to commit {commit}'.format(commit=commit_hash)
